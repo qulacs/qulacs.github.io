84c0902c7c00311d2c8ba496684e059c7485fef6
yibe <ibe@qunasys.com>
2019-10-23 18:28:26 +0900

    v0.1.8

--- search/all_11.js
+++ search/all_11.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['normal',['normal',['../class_random.html#a80038038ac38f0dd01489084e21cf8e2',1,'Random']]],
-  ['normalize',['normalize',['../class_quantum_state_base.html#a86ea604acda07cd2e16c8ef25fee5065',1,'QuantumStateBase::normalize()'],['../class_quantum_state_cpu.html#a59b57ed1bd51ca0f578784cbfc67da2c',1,'QuantumStateCpu::normalize()'],['../class_density_matrix_cpu.html#a4a8ef401def78f78fac2cf9cf69ce78a',1,'DensityMatrixCpu::normalize()']]]
+  ['normalize',['normalize',['../class_quantum_state_base.html#aa71b429a259af3ef96bddb396af32dfd',1,'QuantumStateBase::normalize()'],['../class_quantum_state_cpu.html#af1f928cfbe12c431137e7cb019e5eac0',1,'QuantumStateCpu::normalize()'],['../class_density_matrix_cpu.html#af6a465a8dadb07a5351b3fe04a53f9fe',1,'DensityMatrixCpu::normalize()']]]
 ];
